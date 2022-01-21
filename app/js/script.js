2a7e98466d4828d072e9e3f7b795d0d411e931ec
Daniela B. Fajardo <danielafajardo@ewhain.net>
2022-01-21 13:45:59 +0900

    Header + Hero Section Done!
    That was harder and more-time consuming than expected for someone without experience :)
    
    Working on getting better and improving my skills!

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,7 @@
 const btnHamburger = document.querySelector("#btnHamburger");
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener('click', function() {
     if(header.classList.contains('open')) {
